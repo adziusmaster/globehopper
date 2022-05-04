e3eb91a47a16e1461e5309d494ac8ec40bb62efc
Tim <tim@hoppinger.com>
2022-05-04 08:48:12 +0000

    Fix order of items in menu

--- src/Components/Header/HeaderWidget.tsx
+++ src/Components/Header/HeaderWidget.tsx
@@ -3,7 +3,7 @@ import { HeaderProps } from "./HeaderState";
 
 export const HeaderWidget = (props: HeaderProps): JSX.Element => {
   return (
-    <header className="header header--login">
+    <header className="header">
       <div className="header__inner container">
         <h1 className="header__title">Countries</h1>
       </div>
@@ -11,6 +11,14 @@ export const HeaderWidget = (props: HeaderProps): JSX.Element => {
       <div className="menu">
         <div className="menu__inner container">
           <ul className="menu__list">
+            <li className="menu__item">
+              {ButtonSpecialWidget({
+                key: "all",
+                onClick: props.onAll,
+                classNameButton:  `menu__link${props.currentRoute === 'all' ? ' active' : ''}`,
+                buttontext: "All",
+              })}
+            </li>
             <li className="menu__item">
               {ButtonSpecialWidget({
                 key: "visited",
@@ -41,14 +49,6 @@ export const HeaderWidget = (props: HeaderProps): JSX.Element => {
                 onClick: props.onFavorites,
                 classNameButton:  `menu__link${props.currentRoute === 'favotites' ? ' active' : ''}`,
                 buttontext: "Favorites",
-              })}
-            </li>
-            <li className="menu__item">
-              {ButtonSpecialWidget({
-                key: "all",
-                onClick: props.onAll,
-                classNameButton:  `menu__link${props.currentRoute === 'all' ? ' active' : ''}`,
-                buttontext: "All",
               })}
             </li>
           </ul>
