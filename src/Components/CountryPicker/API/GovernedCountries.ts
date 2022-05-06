e5d8d4fa6d9ff7afa76bf90180876d147bed0a6d
Tim <tim@hoppinger.com>
2022-05-06 07:40:55 +0000

    Add more information to countries in Antarctica

--- src/Components/CountryPicker/API/GovernedCountries.ts
+++ src/Components/CountryPicker/API/GovernedCountries.ts
@@ -1,35 +1,59 @@
 import { GovernedCountries } from "../CountryState";
 
 export const fetchGovernedCountries:GovernedCountries[] = [
+  // FINLAND
   {
     country: 'ALA',
     parentCountry: 'FIN',
-    description: 'Autonomous region of'
+    description: 'Autonomous Region of'
   },
+  // DENMARK
   {
     country: 'FRO',
     parentCountry: 'DNK',
-    description: 'Constituent country of'
+    description: 'Constituent Country of'
   },
   {
     country: 'GRL',
     parentCountry: 'DNK',
-    description: 'Constituent country of'
+    description: 'Constituent Country of'
   },
+  // NORWAY
+  {
+    country: 'SJM-SVA',
+    parentCountry: 'NOR',
+    description: 'Svalbard and Jan Mayen, Remote jurisdiction of'
+  },
+  {
+    country: 'SJM-JMA',
+    parentCountry: 'NOR',
+    description: 'Svalbard and Jan Mayen, Remote jurisdiction of'
+  },
+  {
+    country: 'BVT',
+    parentCountry: 'NOR',
+    description: 'Dependent Territory of'
+  },
+  // UNITED KINGDOM
   {
     country: 'GIB',
     parentCountry: 'GBR',
     description: 'British Overseas Territory'
   },
   {
+    country: 'SGS',
+    parentCountry: 'GBR',
+    description: 'British Overseas Territory'
+  },
+  {
     country: 'GBR-AKR',
     parentCountry: 'GBR',
-    description: 'Sovereign Base Areas of Akrotiri and Dhekelia, British Overseas Territory'
+    description: 'Akrotiri and Dhekelia, British Overseas Territory'
   },
   {
     country: 'GBR-DHE',
     parentCountry: 'GBR',
-    description: 'Sovereign Base Areas of Akrotiri and Dhekelia, British Overseas Territory'
+    description: 'Akrotiri and Dhekelia, British Overseas Territory'
   },
   {
     country: 'GGY',
@@ -46,16 +70,18 @@ export const fetchGovernedCountries:GovernedCountries[] = [
     parentCountry: 'GBR',
     description: 'Crown Dependency of'
   },
+  // FRANCE
   {
-    country: 'SJM-SVA',
-    parentCountry: 'NOR',
-    description: 'Svalvard and Jan Mayen, Remote jurisdiction of'
+    country: 'ATF',
+    parentCountry: 'FRA',
+    description: 'Overseas Territory of'
   },
+  // AUSTRALIA
   {
-    country: 'SJM-JMA',
-    parentCountry: 'NOR',
-    description: 'Svalvard and Jan Mayen, Remote jurisdiction of'
-  }
+    country: 'HMD',
+    parentCountry: 'AUS',
+    description: 'External Territory of'
+  },
 ]
 
 export default fetchGovernedCountries;
